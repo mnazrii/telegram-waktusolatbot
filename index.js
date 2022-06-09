82e55e1e3d0b3e26e4c6d404cbb4d3ac612817e0
unknown <nazri@L-PL10096963.tnb.my>
2022-06-10 02:28:48 +0800

    update to use telegraf api

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
 var axios = require('axios')
 var fs = require('fs')
-
+const { Telegraf } = require('telegraf')
 const schedule = require('node-schedule')
+const { TOKEN,CHAT_ID } = require('./secret')
+const bot = new Telegraf(TOKEN);
+
 
 var text1 = "Telah masuk waktu solat <b>Fardu {0}</b> bagi kawasan Kuala Lumpur pada {1}";
 var waktu = ["Subuh", "Zohor", "Asar", "Magrib", "Isyak",];
@@ -12,120 +15,51 @@ var waktu = ["Subuh", "Zohor", "Asar", "Magrib", "Isyak",];
 var date = new Date();
 var filename = date.getFullYear() + '-data.json'
 var prayerscheduler = [];
-var token,chat_id;
-/**
- * Get prayer time data from esolat **website
- */
-var getprayertimes = async () => {
-    res = await axios.get(
-        'https://www.e-solat.gov.my/index.php?r=esolatApi/takwimsolat&period=year&zone=WLY01',
-    )
-
-    fs.writeFileSync(filename, JSON.stringify(res.data))
-    console.debug(`download latest file ${filename}`)
-}
-
-/**
- * Send notification via telegram
- */
-var notificationCallback = async (text = "negaraku", chat_id = "-725570817") => {
-
-    res = await axios.get(
-        `https://api.telegram.org/bot${token}/sendMessage?parse_mode=HTML&chat_id=${chat_id}&text=${text}`
-    );
-    console.debug(`send notification ${JSON.stringify(res.data)}`)
-}
-
-var imglinks = [
-    'https://pbs.twimg.com/media/E0Y_yVAVIAQWjm4.jpg',
-    `https://i.ibb.co/H7jxp4H/DOPs-WJUEAYdez-W.jpg`,
-    `https://i.ibb.co/fx9JBmD/14925766-1305755386132686-4767847906450935713-n.jpg`,
-    'https://i.ibb.co/wyPtNyH/3-2-30.jpg',
-    'https://i.ibb.co/jRDqmm5/IMG-20200811-071315.jpg',
-
-]
+var alertcheduler = [];
 
 
-/**
- * Send notification via telegram with image
- */
-var notificationImgCallback = async (url, imgcaption = "", chat_id = "-725570817") => {
-
-    res = await axios.get(
-        `https://api.telegram.org/bot${token}/sendPhoto?parse_mode=HTML&chat_id=${chat_id}&photo=${encodeURI(url)}&caption=${imgcaption}`
-    )
-
-    console.debug(`send notification image ${JSON.stringify(res.data)}`)
-}
-
-/**
- * Read downloaded prayer time for particular date
- */
-var readprayertime = async (date) => {
-    timtable = JSON.parse(fs.readFileSync(filename))
-    var dateday = daysIntoYear(date)
-
-    var ret = timtable.prayerTime[dateday - 1];
-    console.debug(`return prayer data ${JSON.stringify(ret)}`)
-    return ret;
-}
-
-/**
- * Convert date to day index of the year. 1/1/2022 will return 0; 31/12/2022 will return 364
- */
-var daysIntoYear = (date) => {
-    var daysIntoYear = (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
-        Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
-
-    console.debug(`return daysIntoYear  ${daysIntoYear}`);
-    return daysIntoYear;
-}
-
-/**
- * Update prayer time scheduler. Send notification on scheduled time
- * 5 times a day
- */
-var updateprayertime = async () => {
-    if (fs.existsSync(filename)) {
-
-        var praydata1 = await readprayertime(date)
-        var prayertimetoday = [praydata1.fajr, praydata1.dhuhr, praydata1.asr, praydata1.maghrib, praydata1.isha];
-        var praydatatime = prayertimetoday.map((x) => x.split(':'));
+const main = async () => {
 
 
-        for (let index = 0; index < prayertimetoday.length; index++) {
-
-            prayerscheduler[index] = schedule.scheduleJob(`${praydatatime[index][1]} ${praydatatime[index][0]} * * *`, () =>
-                notificationImgCallback(imglinks[index], text1.replace("{0}", waktu[index]).replace("{1}", prayertimetoday[index]))
-            )
-            console.debug(`set scheduler ${waktu[index]} - ${praydatatime[index][1]} ${praydatatime[index][0]} * * *`);
-
-        }
-
-    }
+    console.debug(`current date ${date.toDateString()}`);
 
 
+//     await bot.telegram.sendMessage(CHAT_ID,`
+//     Saya akan send reminder <b>10 minit sebelum solat Magrib, Isyak dan Subuh</b> setiap hari.
+//     Sesiapa yang perasan dan tengah free tu moh la kita turun ya.. 
+//     InsyaAllah hati akan lebih tenang.
 
-}
-    ;
+//     <b>Terima kasih</b>
+// `.replaceAll(`    `,''),{parse_mode: 'HTML'})
 
-(async () => {
 
 
     // var praydata1 = await readprayertime(date)
     // var prayertimetoday = [praydata1.fajr, praydata1.dhuhr, praydata1.asr, praydata1.maghrib, praydata1.isha];
     // var praydatatime = prayertimetoday.map((x) => x.split(':'));
+    // // console.log (praydatatime.fajr);
+    //         //10 mins before reminder subuh [0],magrib [3],isyak [4]
+    //         [praydatatime[0], praydatatime[3], praydatatime[4]].forEach(pdreminder => {
+    //             let timetmp = Number(pdreminder[0])*60+Number(pdreminder[1]) - 10;
+                
+    //             //if 0000 hours
+    //             if(timetmp<0)
+    //                 timetmp = timetmp+1440;
+                
+    //             let timetmparr = [parseInt(timetmp / 60), timetmp % 60]; 
+    //             // let schstr = `${pdreminder[1]} ${pdreminder[0]} * * *`;
+    //             let schstr2 = `${timetmparr[1]} ${timetmparr[0]} * * *`;
+                
+    //             console.log (schstr2);
+    //         });
+
     // for (let index = 0; index < prayertimetoday.length; index++) {
-    //     notificationImgCallback(imglinks[index], text1.replace("{0}", waktu[index]).replace("{1}", prayertimetoday[index]));
+    //     abc = await bot.telegram.sendMessage(CHAT_ID,text1.replace("{0}", waktu[index]).replace("{1}", prayertimetoday[index]),{parse_mode: 'HTML'})
     //     console.debug(`set scheduler ${waktu[index]} -  ${praydatatime[index][1]} ${praydatatime[index][0]} * * *`);
     // }
     // return;
 
-    var secret = JSON.parse(fs.readFileSync("secret.json"));
-    token = secret.token;
-    chat_id = secret.chat_id;
 
-    console.debug(`current date ${date.toDateString()}`);
 
     //get latest prayer data if not yet done
     if (!fs.existsSync(filename)) {
@@ -149,4 +83,109 @@ var updateprayertime = async () => {
     })
     console.debug(`set updateprayert scheduler 0 0 * * *`);
 
-})()
+}
+
+
+
+
+
+/**
+ * Get prayer time data from esolat **website write to xxxx-data.json
+ */
+ const getprayertimes = async () => {
+    res = await axios.get(
+        'https://www.e-solat.gov.my/index.php?r=esolatApi/takwimsolat&period=year&zone=WLY01',
+    )
+
+    fs.writeFileSync(filename, JSON.stringify(res.data))
+    console.debug(`download latest file ${filename}`)
+}
+
+
+
+/**
+ * Read downloaded prayer time for particular date
+ */
+ const readprayertime = async (date) => {
+    timtable = JSON.parse(fs.readFileSync(filename))
+    var dateday = daysIntoYear(date)
+
+    var ret = timtable.prayerTime[dateday - 1];
+    console.debug(`return prayer data ${JSON.stringify(ret)}`)
+    return ret;
+}
+
+/**
+ * Convert date to day index of the year. 1/1/2022 will return 0; 31/12/2022 will return 364
+ */
+ const daysIntoYear = (date) => {
+    var daysIntoYear = (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) -
+        Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
+
+    console.debug(`return daysIntoYear  ${daysIntoYear}`);
+    return daysIntoYear;
+}
+
+/**
+ * Update prayer time scheduler. Send notification on scheduled time
+ * 5 times a day
+ */
+ const updateprayertime = async () => {
+
+        var praydata1 = await readprayertime(date)
+        var prayertimetoday = [praydata1.fajr, praydata1.dhuhr, praydata1.asr, praydata1.maghrib, praydata1.isha];
+        var praydatatime = prayertimetoday.map((x) => x.split(':'));
+
+        //schedule prayer time notification
+        for (let index = 0; index < prayertimetoday.length; index++) {
+            
+            let schstr = `${praydatatime[index][1]} ${praydatatime[index][0]} * * *`
+            prayerscheduler[index] = schedule.scheduleJob(schstr, () => //{}
+                bot.telegram.sendMessage(CHAT_ID,text1.replace("{0}", waktu[index]).replace("{1}", prayertimetoday[index]),{parse_mode: 'HTML'})
+            )
+            console.debug(`set scheduler ${waktu[index]} - ${schstr}`);
+
+        }
+        
+
+        //schedule 10 mins before reminder
+        let i = 0;
+        const alerptime = { 
+            "Subuh":praydatatime[0], 
+            "Maghrib":praydatatime[3], 
+            "Ishak": praydatatime[4]
+        }
+
+        Object.keys(alerptime).forEach((key) => {
+
+            //convert to mins, -10 mins
+            let timetmp = Number(alerptime[key][0])*60+Number(alerptime[key][1]) - 10;
+            
+            //if 0000 hours
+            if(timetmp<0)
+                timetmp = timetmp+1440;
+            
+            //convert back to hr:mm
+            let timetmparr = [parseInt(timetmp / 60), timetmp % 60]; 
+            
+            //set task scheduler
+            let schstr2 = `${timetmparr[1]} ${timetmparr[0]} * * *`;
+            
+            let msgstr = `Waktu solat <b>Fardu ${key} </b>akan masuk dalam masa 10 minit. Jom solat berjemaah di surau kita!`;
+            console.debug(msgstr)
+
+            alertcheduler[++i] = schedule.scheduleJob(schstr2, () =>  //{}
+                bot.telegram.sendMessage(CHAT_ID, msgstr, {parse_mode: 'HTML'})
+            )
+            console.debug(`set reminder scheduler - ${schstr2} ${alerptime[key].join(":")}  ${timetmparr.join(":")} `);
+        })
+
+
+    
+
+
+
+}
+
+
+main();
